b8bbb7a662b96c067bef66719c12419b0cde30fb
Mohit Kumar Yadav <mohit.yadav@cobalt.io>
2025-02-27 13:18:51 +0530

    Add stats end point

--- app/blueprints/search_blueprint.py
+++ app/blueprints/search_blueprint.py
@@ -2,14 +2,18 @@ import os
 
 from flask import request, jsonify, Blueprint
 from elasticsearch import Elasticsearch
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 from app.analytics.decorators import log_execution_time
 from app.analytics.logger import LOG
 from app.utils.parsers import parse_params_to_es_body, parse_es_result_to_json
 from app.utils.validations import validate_query_text
 
+# To not overengineer it, I'm just tracking it in memory 🫣
+# A proper solution can be a db or a cache
 query_frequencies = defaultdict(int)
+search_stats = Counter()
+
 search_blueprint = Blueprint('search_blueprint', __name__)
 
 @search_blueprint.route("/search", methods=['GET'])
@@ -39,15 +43,22 @@ def search():
 
     try:
         results = es_client.search(index='imago', body=body)
+        search_stats['successful'] += 1
         return parse_es_result_to_json(results, page)
     except ConnectionError as e:
+        search_stats['failed'] += 1
+        LOG.error(f"Error connecting to Elasticsearch: {e}")
         return jsonify({'error': 'Connection error to Elasticsearch'}), 500
     except Exception as e:
+        search_stats['failed'] += 1
         LOG.error(f"Error processing search: {e}")
         return jsonify({'error': str(e)}), 500
 
-
 @search_blueprint.route("/search/stats", methods=['GET'])
 def get_most_common_queries():
-    return [{'query': k, 'count': v} for k, v in sorted(query_frequencies.items(),
-                                                        key=lambda x: x[1], reverse=True)[:5]]
+    top_five_queries = [{'query': k, 'count': v} for k, v in sorted(query_frequencies.items(),
+                                                                    key=lambda x: x[1], reverse=True)[:5]]
+    return {
+        'top_five_queries': top_five_queries,
+        'search_stats': dict(search_stats)
+    }
