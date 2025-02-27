717c77813520a139b4f683b83f4a88d726b8f97e
Mohit Kumar Yadav <mohit.yadav@cobalt.io>
2025-02-27 12:09:43 +0530

    Add test for search endpoint

---
+++ tests/blueprints/search_blueprint.py
@@ -0,0 +1,48 @@
+import unittest
+from unittest.mock import patch
+
+from app import app
+
+
+class TestSearchEndpoint(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    @patch.dict('os.environ', {
+        'ELASTIC_URL': 'http://mock-elastic-url.com:2033',
+        'ELASTIC_USERNAME': 'mock-username',
+        'ELASTIC_PASSWORD': 'mock-password'
+    })
+    @patch('elasticsearch.Elasticsearch.search')
+    def test_search_connection_error(self, mock_es):
+        response = self.app.get('/search')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json, {'error': 'Search query is required'})
+
+    @patch.dict('os.environ', {
+        'ELASTIC_URL': 'http://mock-elastic-url.com:2033',
+        'ELASTIC_USERNAME': 'mock-username',
+        'ELASTIC_PASSWORD': 'mock-password'
+    })
+    @patch('elasticsearch.Elasticsearch.search')
+    def test_search_successful(self, mock_es):
+        mock_es.return_value = {
+            'hits': {
+                'total': {'value': 2},
+                'hits': [
+                    {'_source': {'suchtext': 'Example result'}},
+                    {'_source': {'suchtext': 'Another result'}}
+                ]
+            }
+        }
+
+        response = self.app.get('/search?q=Big%20Sur')
+        data = response.json
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['page'], 1)
+        self.assertEqual(data['total_results'], 2)
+
+if __name__ == '__main__':
+    unittest.main()
